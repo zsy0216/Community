12c8015131832931953188bd459aa2f382176b44
zsy0216 <594983498@qq.com>
2020-09-11 16:17:14 +0800

    refactor: 重构前端点赞逻辑，加入某用户获赞量

--- src/main/resources/static/js/discuss.js
+++ src/main/resources/static/js/discuss.js
@@ -1,7 +1,7 @@
-function like(btn, entityType, entityId) {
+function like(btn, entityType, entityId, emtityUserId) {
     $.post(
         "/like",
-        {"entityType": entityType, "entityId": entityId},
+        {"entityType": entityType, "entityId": entityId, "entityUserId": emtityUserId},
         function (data) {
             data = $.parseJSON(data);
             if (data.code === 0) {
