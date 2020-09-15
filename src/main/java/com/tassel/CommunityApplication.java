d83ed5e5c71c8b0f5fa6e2cb782a3177d59924b1
zsy0216 <594983498@qq.com>
2020-09-15 16:20:02 +0800

    feat: 整合 elasticsearch

--- src/main/java/com/tassel/CommunityApplication.java
+++ src/main/java/com/tassel/CommunityApplication.java
@@ -3,11 +3,20 @@ package com.tassel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 /**
  * @author Ep流苏
  */
 @SpringBootApplication
 public class CommunityApplication {
+
+    @PostConstruct
+    public void init() {
+        // 解决 Netty 启动冲突问题
+        // see Netty4Utils.setAvailableProcessors()
+        System.setProperty("es.set.netty.runtime.available.processors", "false");
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CommunityApplication.class, args);
